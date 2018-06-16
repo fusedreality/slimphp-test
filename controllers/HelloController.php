15cd9d2593c27f62cd20e586fc1d3924951199fe
Craig McCreath <craig.mccreath@mtcmedia.co.uk>
2018-06-16 01:08:52 +0100

    Adding PDO and config structure

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -2,26 +2,46 @@
 
 namespace Controllers;
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 /**
- * Used to show how we can also use controllers to do same actions.
+ * Class HelloController
+ *
+ * @package Controllers
  */
 class HelloController
 {
+    /**
+     * @var \Slim\Container Stores the container for dependency purposes.
+     */
     protected $container;
 
+
     /**
-     * Set up this controller with neccessary container
+     * Store the container during class construction.
+     *
+     * @param \Slim\Container $container
      */
-    public function __construct(\Slim\Container $container) {
+    public function __construct(\Slim\Container $container)
+    {
         $this->container = $container;
     }
 
     /**
-     * Handle the 'Goodbye' event
+     * Output a hello message to a specified name.
+     *
+     * @param  Request  $request
+     * @param  Response $response
+     * @param  Array    $args
+     * @return mixed
      */
-    public function index($request, $response, $args) {
-      return $this->container->get('view')->render($response, 'name.twig', [
-        'name' => $args['name']
-      ]);
+    public function index(Request $request, Response $response, $args)
+    {
+        return $this->container->get('view')->render(
+            $response, 'name.twig', [
+            'name' => $args['name']
+            ]
+        );
     }
 }
--- index.php
+++ index.php
@@ -1,35 +1,8 @@
 <?php
 
 require_once 'vendor/autoload.php';
-
-// Create app
-$app = new \Slim\App();
-
-// Get container
-$container = $app->getContainer();
-
-// Register component on container
-$container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig('templates', [
-        'cache' => false
-    ]);
-
-    // Instantiate and add Slim specific extension
-    $basePath = rtrim(str_ireplace('index.php', '', $container->get('request')->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
-
-    return $view;
-};
-
-// Render Twig template in route
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $this->view->render($response, 'name.twig', [
-        'name' => $args['name']
-    ]);
-})->setName('profile');
-
-// Render Twig template within container
-$app->get('/goodbye/{name}', 'Controllers\\HelloController:index');
+require_once 'bootstrap.php';
+require_once 'routes.php';
 
 // Run app
 $app->run();
